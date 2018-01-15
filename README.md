79e7ee9614a38a25b4a472599fe465b1a3d7e9db
XiaXianzhuo <xiaxzh@mail2.sysu.com>
2018-01-15 23:24:50 +0800

    Merge the team work

--- README.md
+++ README.md
@@ -5,3 +5,4 @@
 * [cloudgo](cloudgo/README.md)
 * [cloudgo-inout](cloudgo-inout/README.md)
 * [cloudgo-data](cloudgo-data/README.md)
+* [service-agenda](service-agenda/README-xiaxzh.md)
--- cloudgo-data/entities/initial.go
+++ cloudgo-data/entities/initial.go
@@ -9,19 +9,19 @@ import (
 var mydb *sql.DB
 
 // use godbc if define db
-func init() {
-	db, err := sql.Open("mysql", "root:root@tcp(127.0.0.1:3306)/test?charset=utf8&parseTime=true")
-	checkErr(err)
-	mydb = db
-}
+// func init() {
+// 	db, err := sql.Open("mysql", "root:root@tcp(127.0.0.1:3306)/test?charset=utf8&parseTime=true")
+// 	checkErr(err)
+// 	mydb = db
+// }
 
 var myOrm *xorm.Engine
 // use xorm if define engine
-// func init() {
-// 	engine, err := xorm.NewEngine("mysql", "root:root@tcp(127.0.0.1:3306)/test?charset=utf8&parseTime=true")
-// 	checkErr(err)
-// 	myOrm = engine
-// }
+func init() {
+	engine, err := xorm.NewEngine("mysql", "root:root@tcp(127.0.0.1:3306)/test?charset=utf8&parseTime=true")
+	checkErr(err)
+	myOrm = engine
+}
 
 type SQLExecer interface {
 	Exec(query string, args ...interface{}) (sql.Result, error)
